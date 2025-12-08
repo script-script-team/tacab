54570ea03e6b5dbee6076f0e97063d0715efc5cb
DrZiMo <zuhaybhamar@gmail.com>
2025-12-08 06:04:08 +0300

    feat: Add `ToatalIncome` component to display total income statistics using a radial chart.

--- tacab_admin_frontend/src/components/income.tsx
+++ tacab_admin_frontend/src/components/income.tsx
@@ -1,74 +1,72 @@
-"use client"
+'use client'
 
-import { TrendingUp } from "lucide-react"
-import { Label, PolarRadiusAxis, RadialBar, RadialBarChart } from "recharts"
+import { Label, PolarRadiusAxis, RadialBar, RadialBarChart } from 'recharts'
 
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from '@/components/ui/card'
 import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
   type ChartConfig,
-} from "@/components/ui/chart"
-import { useComIncome, useItIncome } from "@/react-query/dashboard.hooks"
-import { useEffect } from "react"
-import { toast } from "sonner"
-import Loading from "./Loading"
+} from '@/components/ui/chart'
+import { useComIncome, useItIncome } from '@/react-query/dashboard.hooks'
+import { useEffect } from 'react'
+import { toast } from 'sonner'
+import Loading from './Loading'
 
-export const description = "A radial chart with stacked sections"
+export const description = 'A radial chart with stacked sections'
 
 export function ToatalIncome() {
+  const it = useItIncome()
+  const com = useComIncome()
 
-  const it = useItIncome();
-  const com = useComIncome();
+  const totalItIncome = it.data?.total.reduce((a, b) => a + b.total, 0)
+  const totalComIncome = com.data?.total.reduce((a, b) => a + b.total, 0)
+  const totalIncome = totalItIncome! + totalComIncome!
 
-  const totalItIncome = it.data?.total.reduce((a, b) => a + b.total, 0);
-  const totalComIncome = com.data?.total.reduce((a, b) => a + b.total, 0);
-  const totalIncome = totalItIncome! + totalComIncome!;
-  const currentIT = it.data?.total[0].total;
-  const currentCOM = com.data?.total[0].total;
-  const currentTotal = currentIT! + currentCOM!;
-  const past = [...(it?.data?.total?.slice(1).map(i => i.total) ?? []), ...(com?.data?.total.slice(1).map(i => i.total) ?? []),];
-  const avg = past.reduce((a, b) => a + b, 0) / past.length;
-  const percentage = avg === 0 ? 100 : ((currentTotal - avg) / avg) * 100;
+  const chartData = [
+    { month: 'january', itIncome: totalItIncome, comIncome: totalComIncome },
+  ]
 
-  const chartData = [{ month: "january", itIncome: totalItIncome, comIncome: totalComIncome }]
-
-const chartConfig = {
-  itIncome: {
-    label: "IT: ",
-    color: "var(--chart-3)",
-  },
-  comIncome: {
-    label: "computer: ",
-    color: "var(--chart-2)",
-  },
-} satisfies ChartConfig
+  const chartConfig = {
+    itIncome: {
+      label: 'IT: ',
+      color: 'var(--chart-3)',
+    },
+    comIncome: {
+      label: 'computer: ',
+      color: 'var(--chart-2)',
+    },
+  } satisfies ChartConfig
 
   useEffect(() => {
-    if(it.isError) toast.error(it.error.message);
-    if(com.isError) toast.error(com.error.message);
-  }, [it.isError, com.isError]);
+    if (it.isError) toast.error(it.error.message)
+    if (com.isError) toast.error(com.error.message)
+  }, [it.isError, com.isError])
 
-  if(it.isLoading) return <div className="w-full h-[50vh] rounded-lg"><Loading /></div>
+  if (it.isLoading)
+    return (
+      <div className='w-full h-[50vh] rounded-lg'>
+        <Loading />
+      </div>
+    )
 
   return (
-    <Card className="flex flex-col">
-      <CardHeader className="items-center pb-0">
+    <Card className='flex flex-col'>
+      <CardHeader className='items-center pb-0'>
         <CardTitle>Total income - statistics</CardTitle>
         <CardDescription>{new Date().toDateString()}</CardDescription>
       </CardHeader>
-      <CardContent className="flex flex-1 items-center pb-0">
+      <CardContent className='flex flex-1 items-center pb-0'>
         <ChartContainer
           config={chartConfig}
-          className="mx-auto aspect-square w-full max-w-[250px]"
+          className='mx-auto aspect-square w-full max-w-[250px]'
         >
           <RadialBarChart
             data={chartData}
@@ -83,20 +81,20 @@ const chartConfig = {
             <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
               <Label
                 content={({ viewBox }) => {
-                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                  if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
                     return (
-                      <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle">
+                      <text x={viewBox.cx} y={viewBox.cy} textAnchor='middle'>
                         <tspan
                           x={viewBox.cx}
                           y={(viewBox.cy || 0) - 16}
-                          className="fill-foreground text-2xl font-bold"
+                          className='fill-foreground text-2xl font-bold'
                         >
                           ${totalIncome.toLocaleString()}
                         </tspan>
                         <tspan
                           x={viewBox.cx}
                           y={(viewBox.cy || 0) + 4}
-                          className="fill-muted-foreground"
+                          className='fill-muted-foreground'
                         >
                           Income
                         </tspan>
@@ -107,30 +105,22 @@ const chartConfig = {
               />
             </PolarRadiusAxis>
             <RadialBar
-              dataKey="itIncome"
-              stackId="a"
+              dataKey='itIncome'
+              stackId='a'
               cornerRadius={5}
-              fill="var(--color-itIncome)"
-              className="stroke-transparent stroke-2"
+              fill='var(--color-itIncome)'
+              className='stroke-transparent stroke-2'
             />
             <RadialBar
-              dataKey="comIncome"
-              fill="var(--color-comIncome)"
-              stackId="a"
+              dataKey='comIncome'
+              fill='var(--color-comIncome)'
+              stackId='a'
               cornerRadius={5}
-              className="stroke-transparent stroke-2"
+              className='stroke-transparent stroke-2'
             />
           </RadialBarChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 leading-none font-medium">
-          Trending up by {percentage}% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="text-muted-foreground leading-none">
-          Showing total income for the last 8 months
-        </div>
-      </CardFooter>
     </Card>
   )
 }
