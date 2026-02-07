b80554e0a3d24461553fd8466ae28e28f228df00
QuantaShade <developer.mustafa1st@gmail.com>
2026-02-06 18:02:19 -0800

    change

--- tacab_admin_frontend/src/react-query/forgotPassword.ts
+++ tacab_admin_frontend/src/react-query/forgotPassword.ts
@@ -1,12 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 import api from './axios';
-import { BASE_API_URL } from '@/pages/constant';
 
 export const useForgotPassword = () => {
   return useMutation({
     mutationKey: ['forgot-password'],
     mutationFn: async (email: string) => {
-      const res = await api.post(`${BASE_API_URL}/api/auth/forgot-password`, { email });
+      const res = await api.post('api/auth/forgot-password', { email });
 
       if (!res.data.ok) {
           throw new Error(
@@ -24,7 +23,7 @@ export const useVerifyResetCode = () => {
     mutationKey: ['verify-reset-code'],
     mutationFn: async (payload: { email: string; code: string }) => {
 
-      const res = await api.post(`${BASE_API_URL}/api/auth/verify-reset-code`, payload);
+      const res = await api.post('api/auth/verify-reset-code', payload);
       if (!res.data.ok) {
           throw new Error(
             res.data.message || 'Failed to verify code'
@@ -40,7 +39,7 @@ export const useResetPassword = () => {
   return useMutation({
     mutationKey: ['reset-password'],
     mutationFn: async (payload: { email: string; password: string }) => {
-      const res = await api.post(`${BASE_API_URL}/api/auth/reset-password`, payload);
+      const res = await api.post('api/auth/reset-password', payload);
       if (!res.data.ok) {
           throw new Error(
             res.data.message || 'Failed to reset password'
