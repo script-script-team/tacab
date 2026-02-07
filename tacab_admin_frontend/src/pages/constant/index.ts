0fb5c7be13654959f452f12fee89b0c09eebf212
DrZiMo <zuhaybhamar@gmail.com>
2026-02-07 19:13:20 +0300

    changed the backend URL

--- tacab_admin_frontend/src/pages/constant/index.ts
+++ tacab_admin_frontend/src/pages/constant/index.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'http://localhost:3001'
-// export const BASE_API_URL = 'https://tacab-backend.onrender.com'
+// export const BASE_API_URL = 'http://localhost:3001'
+export const BASE_API_URL = 'https://tacab-backend.onrender.com'
 export const DEFAULT_ERROR_MESSAGE =
   'Opps! Something went wrong with the server'
